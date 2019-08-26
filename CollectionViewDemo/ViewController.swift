2736ab02892812bfd98e7680e76a626eddf5f3c3
Jiachen_Fu <jiabao510834504@126.com>
2019-08-26 20:51:39 +0800

    Initial Commit

---
+++ CollectionViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewDemo
+//
+//  Created by JiachenFu on 2019/8/26.
+//  Copyright © 2019 Foxconn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
